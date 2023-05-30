831e0bedacbd92c0d6b2dba22c08d031e50edda0
nic <nicholasalmeida567@gmail.com>
2023-05-29 21:27:00 -0300

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
+set -e
+
 CFLAGS="-Wall -Wextra -std=c11 -pedantic"
 LIBS="-lSDL2 -lSDL2_ttf"
 
 gcc $CFLAGS -o gesq ./src/main.c ./src/ui.c ./src/gol.c $LIBS
-RESULT=$?
 
-if [ $RESULT -eq 0 ]; then
-    if [ "$1" = "run" ]; then
-        ./gesq
-    fi
+if [ "$1" = "run" ]; then
+    ./gesq
 fi
